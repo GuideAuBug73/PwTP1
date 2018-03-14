74524286a88d0b64e39daa902aa3a76f9197f15f
mingeonj <jacques7473@gmail.com>
2018-03-14 15:04:10 +0100

    test seb

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  results:Result[];
 
   constructor(public navCtrl: NavController) {
-
+    this.results=fakeresults;
   }
 
 }
+
+export interface Result{
+  title:string;
+  author:string;
+  date:number;
+}
+
+const fakeresults: Result[]=[
+  {title:'Back to the futur',author:'toto',date:'1789'},
+  {title:'test',author:'titi',date:'1997'}
+]
